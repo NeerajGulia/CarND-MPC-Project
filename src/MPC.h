44ff699633a01e3960827c19d28fbcd7e8563fef
Neeraj Gulia <nk.gulia@gmail.com>
2018-04-26 10:18:24 +0530

    increased speed limit to 70MPH from 40MPH
    reduced iterations from 15 to 8.

--- src/MPC.h
+++ src/MPC.h
@@ -10,8 +10,8 @@ class Constants
 {
   public:
     // TODO: Set the timestep length and duration
-    static const size_t N = 15;
-    static constexpr  double DT = 0.1; //seconds
+    static const size_t N = 8;
+    static constexpr  double DT = 0.15; //seconds
     static const int POLY_ORDER = 3;
 
     // This value assumes the model presented in the classroom is used.
@@ -29,7 +29,7 @@ class Constants
     static constexpr double FACTOR_MILES_TO_METER_PER_SEC = 0.44704;
     // Both the reference cross track and orientation errors are 0.
     // The reference velocity is set to 40 mph.
-    static constexpr  double REF_V = 40 * FACTOR_MILES_TO_METER_PER_SEC; //convert from m/h to m/s
+    static constexpr  double REF_V = 70 * FACTOR_MILES_TO_METER_PER_SEC; //convert from m/h to m/s
     
     // The solver takes all the state variables and actuator
     // variables in a singular vector. Thus, we should to establish
